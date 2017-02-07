7f76bd5185e40e0597b4929739784a1e13bc5082
myztajay <myztajay@hotmail.com>
2017-02-07 13:16:10 -0500

    Created pages controller and made default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root 'pages#index'
+
+  get 'pages/profile'
+
+  get 'pages/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
 end
