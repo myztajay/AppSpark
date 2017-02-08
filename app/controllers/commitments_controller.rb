30ddf01a23f370e3cf31ee0ad7da0474f6294c0f
myztajay <myztajay@hotmail.com>
2017-02-08 13:52:41 -0500

    Added commitments section to app show page succesfully added logic to pass right paramenters to save commitments

--- app/controllers/commitments_controller.rb
+++ app/controllers/commitments_controller.rb
@@ -1,7 +1,17 @@
 class CommitmentsController < ApplicationController
   def create
+
+    @commitment = Commitment.new(commitment_parameters)
+    @commitment.user = current_user
+    @commitment.save
   end
 
   def destroy
+  end
+
+  private
+
+  def commitment_parameters
+    params.require(:commitment).permit(:app_id)
   end
 end
