0918ee898b603134dc4f25244462a2dff56738b1
myztajay <myztajay@hotmail.com>
2017-02-12 12:14:26 -0500

    Created user show/profile, Deleted pages controller as this data will be recreated as a static landing page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,16 @@
 Rails.application.routes.draw do
-
+  get 'users/show'
   get 'commitments/create'
-
   get 'commitments/destroy'
 
   resources :apps
   resources :userskills, only: [:create, :destroy]
   resources :commitments, only: [:create, :destroy]
+  resources :users, only: [:show]
   devise_for :users
   root 'apps#index'
 
-
   get 'pages/profile'
-
   get 'pages/show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
