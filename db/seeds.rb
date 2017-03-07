2f01b8801d065d238a60e9220e4e88bab2f15b11
myztajay <myztajay@hotmail.com>
2017-03-06 21:00:53 -0500

    Added seed data for skills

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Skill.destroy_all
+
+Skill.create(skill: "Javascript")
+Skill.create(skill: "Ruby")
+Skill.create(skill: "Python")
+Skill.create(skill: "Java")
+Skill.create(skill: "C++")
+Skill.create(skill: "Assembly")
+Skill.create(skill: "Cobalt")
+Skill.create(skill: "Angularjs")
+Skill.create(skill: "Nodejs")
+Skill.create(skill: "Reactjs")
