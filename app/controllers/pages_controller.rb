6b5510dcfb3b52006fab073fa31dda8bf772cc5e
myztajay <myztajay@hotmail.com>
2017-11-10 22:00:28 +0000

    Added pool page and did some initial styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,8 @@ class PagesController < ApplicationController
     @apps = App.first(6)
     @pop_apps = App.first(3)
   end
+  
+  def pool
+    @users = User.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'pages/index'
+  get 'pages/pool'
 
   #taging path
   get 'tags/:tag', to: 'apps#index', as: :tag
@@ -8,14 +9,15 @@ Rails.application.routes.draw do
   resources :apps
   resources :userskills, only: [:create, :destroy]
   resources :commitments, only: [:create, :destroy]
+  
   devise_for :users
   # Must go after more specific devise routes
   resources :users, only: [:show, :update]
   # get 'users/profile/:id', to: 'users#show', as: :user
+  
   #omniauth
   get "auth/github/", to: "apps#index"
   match "auth/github/callback" => "users#github_data", :via => [:get, :post]
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
